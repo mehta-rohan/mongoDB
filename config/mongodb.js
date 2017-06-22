3dd8a8592b28acaf050c60280105e4e4a733c429
Rohan Mehta <rohanmehta0077@gmail.com>
2017-06-22 16:53:54 +0530

    Initial Commit

---
+++ config/mongodb.js
@@ -0,0 +1,39 @@
+//const MongoClient = require('mongodb').MongoClient;
+
+const {MongoClient,ObjectID} = require('mongodb');
+
+console.log(new ObjectID());
+
+var user  = {name : 'rohan',age : 26};
+
+var {name} = user;
+console.log(name);
+/*MongoClient.connect('mongodb://localhost:27017/todoApp',(err,db)=>{
+	if(err){
+		return console.log(err);
+	}
+	db.collection('Todos').insertOne(
+		{
+			text:"Psssssttt.."
+		},(err,res)=>{
+		if(err){
+			return console.log(err);
+		}
+		console.log(res.ops[0]._id.getTimestamp());
+	});
+	db.close();
+});*/
+
+
+MongoClient.connect('mongodb://localhost:27017/todoApp',(err,db)=>{
+	if(err){
+		return console.log(err);
+	}
+	db.collection('Todos').find().toArray().then((docs)=>{
+		console.log(docs);
+	},(err)=>{
+		console.log(err);
+	});
+//	db.close();
+	return;
+});
